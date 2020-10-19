fad2d233f371c28476c525e9c6123b785bf5fd33
touchDeeper <610690824@qq.com>
2020-10-19 17:40:27 +0800

    add tf class tool

---
+++ include/td_ros/publisher/tf_broadcaster.hpp
@@ -0,0 +1,25 @@
+/*
+ * @Description: 发布tf的类
+ * @Author: Ren Qian
+ * @Date: 2020-03-05 15:23:26
+ */
+
+
+#pragma once
+#include <string>
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+#include <Eigen/Dense>
+
+namespace td_ros {
+class TFBroadCaster {
+  public:
+    TFBroadCaster(std::string frame_id, std::string child_frame_id);
+    TFBroadCaster() = default;
+    void SendTransform(Eigen::Matrix4f pose, double time);
+  protected:
+    tf::StampedTransform transform_;
+    tf::TransformBroadcaster broadcaster_;
+};
+}
